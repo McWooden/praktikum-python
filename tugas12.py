910f0c4c9c08382d6c114f2b3bcb55f7cf534fab
Huddin <96611341+McWooden@users.noreply.github.com>
2025-11-18 06:37:04 +0000

    simple aja

--- tugas12.py
+++ tugas12.py
@@ -1,3 +1,9 @@
+# Link Tugas https://drive.google.com/file/d/1P8BTsMXpr0thyzCeOfeHYGOu_E1FuX5i/view
+# Materinya udah aku jelasin di Medium sebelumnya: https://medium.com/@halohuddin/modul-rakitan-fun-fun-function-10f159abfe76
+
+# Note: Simple aja, kalian tinggal bikin fungsi sesuai instruksi di artikel Medium di atas
+# Cara bikinnya tinggal bikin terima input dari user terus return rumusnya
+
 def luas_persegi_panjang():
     print("Waktunya menghitung luas persegi panjang")
 
@@ -26,4 +32,8 @@ def volume_bola():
     jari_jari = int(input("Jari-jari\t:"))
     return .75 * 3.14 * jari_jari**3
 
-print(luas_lingkaran())
+# Contoh pemanggilan fungsi
+print("Luas Persegi Panjang:", luas_persegi_panjang())
+print("Volume Balok:", volume_balok())
+print("Luas Lingkaran:", luas_lingkaran())
+print("Volume Bola:", volume_bola())
